603ea60660dfc263cb43213931cb65b2151ddea0
neerajsb <neerajsbadamikar@gmail.com>
2018-01-16 17:43:55 -0500

    Update springMassDamperDesign.m

--- source/springMassDamperDesign.m
+++ source/springMassDamperDesign.m
@@ -2,10 +2,8 @@ function design = springMassDamperDesign(mass)
 
 if nargin
   m = mass;
-  randomVar = rand;
 else
   m = 1500; % Need to know the mass to determine critical damping
-  randomVar = rand;
 end
 
 design.k = 5e6;                  % Spring Constant
