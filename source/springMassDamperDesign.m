00ecea67ecbff2ac4b4334f9f19fd06147bd18ea
neerajsb <neerajsbadamikar@gmail.com>
2018-01-17 10:36:32 -0500

    Update springMassDamperDesign.m

--- source/springMassDamperDesign.m
+++ source/springMassDamperDesign.m
@@ -5,7 +5,6 @@ if nargin
 else
   m = 1500; % Need to know the mass to determine critical damping
 end
-k=5;
 
 design.k = 5e6;                  % Spring Constant
 design.c = 2*m*sqrt(design.k/m); % Damping Coefficient to be critically damped
